5738b0b25c161167a692feab5a9e9e96fd8971fc
Dharmendra Singh <dhrmpaji9690@gmail.com>
2023-06-13 05:42:08 -0700

    UPDATE : Saving Data

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
+let tasks = [];
+
+// Retrieve tasks from local storage on page load
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    renderTasks();
+  }
+});
 
 addButton.addEventListener('click', () => {
   const taskText = taskInput.value;
 
   if (taskText.trim() !== '') {
-    const taskItem = document.createElement('li');
-    taskItem.innerText = taskText;
+    const taskItem = {
+      text: taskText,
+      completed: false
+    };
 
-    taskItem.addEventListener('click', () => {
-      taskItem.classList.toggle('completed');
-    });
+    tasks.push(taskItem);
+    saveTasksToLocalStorage();
+    renderTasks();
 
-    taskItem.addEventListener('contextmenu', (e) => {
-      e.preventDefault();
-      taskItem.remove();
-    });
-
-    taskList.appendChild(taskItem);
     taskInput.value = '';
   }
 });
+
+taskList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    const selectedTaskIndex = Array.from(taskList.children).indexOf(event.target);
+    tasks[selectedTaskIndex].completed = !tasks[selectedTaskIndex].completed;
+
+    saveTasksToLocalStorage();
+    renderTasks();
+  }
+});
+
+taskList.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  if (event.target.tagName === 'LI') {
+    const selectedTaskIndex = Array.from(taskList.children).indexOf(event.target);
+    tasks.splice(selectedTaskIndex, 1);
+
+    saveTasksToLocalStorage();
+    renderTasks();
+  }
+});
+
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((taskItem) => {
+    const taskElement = document.createElement('li');
+    taskElement.innerText = taskItem.text;
+
+    if (taskItem.completed) {
+      taskElement.classList.add('completed');
+    }
+
+    taskList.appendChild(taskElement);
+  });
+}
+
+function saveTasksToLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
